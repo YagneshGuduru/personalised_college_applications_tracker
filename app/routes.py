436a9e6d14317e4f31d551d5defec0c6546ecc14
YagneshGuduru <yagnesh2403@gmail.com>
2025-11-06 23:26:38 +0530

    Created dashboard - an entry point to webapplication.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,20 @@
+from flask import render_template
+from app.db import db_connection
+
 def init_app(app):
+
     @app.route("/")
-    def home():
-        return "App running!"
+    def dashboard():
+        conn = db_connection()
+        cursor = conn.cursor()
+
+        # Get distinct list of countries.
+        cursor.execute(''' 
+                        SELECT country_name, COUNT(*) as count 
+                        FROM applications
+                        GROUP BY country_name
+                    ''')
+        country_data = cursor.fetchall()
+        countries = [(row["country_name"], row['count']) for row in country_data]
+
+        return render_template("dashboard.html", countries = countries)
